68ad5096a3afe1483584b5ade0d8d0b071464e78
Sarah Bindman <sarahbindman@gmail.com>
2015-03-21 23:15:56 -0700

    updated port for pushing

--- server.py
+++ server.py
@@ -12,5 +12,8 @@ def index():
     return render_template("map.html")
 
 
+PORT=int(os.environ.get("PORT",5000))
+
+
 if __name__ == "__main__":
-    app.run( debug=True)
+    app.run( debug=False, host="0.0.0.0" ,port=PORT)
